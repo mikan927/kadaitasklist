9c1f01db2b1ec458df12233d52ce1a9d25384565
EC2 Default User <ec2-user@ip-172-31-47-172.us-east-2.compute.internal>
2018-03-05 02:03:49 +0000

    user login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,13 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
+
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
